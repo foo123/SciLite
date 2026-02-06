2831fc5e413ca5dfb36d82ce8a557f8f7c969942
Nikos M <workingclasscode@gmail.com>
2026-02-06 13:51:35 +0200

    fn.squeeze

--- src/fn/core/squeeze.js
+++ src/fn/core/squeeze.js
@@ -1,8 +1,19 @@
-function squeeze(x)
+function squeeze(x, dim)
 {
     if (is_array(x))
     {
-        return 1 === x.length ? squeeze(x[0]) : array(x.length, function(i) {return squeeze(x[i]);});
+        if (null == dim) dim = 1;
+        if ((1 === x.length) && (2 < dim))
+        {
+            // squeeze only higher dimensions than 2
+            return squeeze(x[0], dim+1);
+        }
+        else
+        {
+            return x.map(function(xi) {
+                return squeeze(xi, dim+1);
+            });
+        }
     }
     else
     {
