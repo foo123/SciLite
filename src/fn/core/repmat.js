dcf670a48b91ddcd746173db44e4954a1b59f18d
Nikos M <workingclasscode@gmail.com>
2026-04-04 00:14:52 +0300

    fix repmat

--- src/fn/core/repmat.js
+++ src/fn/core/repmat.js
@@ -4,11 +4,17 @@ function repmat(x)
     if (is_array(n[0])) n = n[0];
     n = n.map(_);
     if (1 === n.length) n = array(sz.length, n[0]);
+    if (is_array(x))
+    {
+        if (sz.length < n.length) sz = sz.concat(array(n.length-sz.length, 1));
+        else if (n.length < sz.length) n = n.concat(array(sz.length-n.length, 1));
+    }
     if (!is_array(x) || (sz.length === n.length))
     {
+        if (is_vector(x)) x = [x];
         return ndarray(is_array(x) ? n.map(function(ni, i) {return sz[i]*ni;}) : n, function(indices) {
             return indices.reduce(function(arr, index, dim) {
-                return is_array(arr) ? arr[index % n[dim]] : arr;
+                return is_array(arr) ? arr[index % sz[dim]] : arr;
             }, x);
         });
     }
